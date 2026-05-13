da4757327abd37c5f8b3c2a220f04f25e290b109
masudrana-prog <masudrana53913@gmail.com>
2026-05-13 22:21:27 +0600

    register admin

--- campus/admin.py
+++ campus/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Campus
+from .models import Campus, Canteen, MenuItem, Order, OrderItem, Event, LostAndFound, Classroom
+
 admin.site.register(Campus)
+admin.site.register(Canteen)
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(Event)
+admin.site.register(LostAndFound)
+admin.site.register(Classroom)
