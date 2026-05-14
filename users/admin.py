679376e7f6a7d26faedb3110d3e614f12a8e4f39
23201192-sudo <23201192@uap-bd.edu>
2026-05-14 15:12:27 +0600

    users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, StudentProfile, TeacherProfile, AlumniProfile
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'username', 'first_name', 'last_name', 'role', 'is_active']
+    list_filter = ['role', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('phone', 'role', 'profile_picture')}),
+    )
+
+admin.site.register(StudentProfile)
+admin.site.register(TeacherProfile)
+admin.site.register(AlumniProfile)
+
+from .models import Faculty, Message
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'department', 'designation', 'rank', 'email', 'is_active', 'order']
+    list_filter = ['department', 'rank', 'is_active']
+    search_fields = ['name', 'department', 'email']
+    list_editable = ['order', 'is_active']
+
+admin.site.register(Message)
